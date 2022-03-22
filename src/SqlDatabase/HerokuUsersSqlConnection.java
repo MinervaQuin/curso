edd91f05bcd4b99638908b8810f115265075b8b1
selenegc01 <99739761+selenegc01@users.noreply.github.com>
2022-03-22 18:32:13 +0000

    Update HerokuUsersSqlConnection.java

--- src/SqlDatabase/HerokuUsersSqlConnection.java
+++ src/SqlDatabase/HerokuUsersSqlConnection.java
@@ -7,7 +7,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import model.user;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -79,7 +81,7 @@ public class HerokuUsersSqlConnection extends SqlConnection{
                 System.out.println("Error al eliminar por id en la tabla USERS: " + e.getMessage());
             }
     }
-        
+     
     public JSONObject getCalendarById(int id) throws ParseException {
         String sql = "SELECT calendar FROM USERS WHERE id=" + Integer.toString(id);
         JSONObject jsonObjectCalendar = null;
@@ -114,4 +116,68 @@ public class HerokuUsersSqlConnection extends SqlConnection{
             }
     }
     
+    public boolean login(String email, String pswd) throws SQLException {
+        // Por alguna razón, da error si el email contiene una '@'
+        // Ejemplo: Si un usuario tiene de email 123@email, aunque exista en
+        // la base de datos, la consulta da error.
+        // Por tanto, probar sin caracteres especiales
+        
+        String sql = "SELECT email, password FROM USERS WHERE email=" + email + " AND password=" + pswd;
+        
+        try (Connection conn = this.connect();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)){
+            while (rs.next()) {
+                System.out.println(rs.getString("email"));
+                System.out.println(rs.getString("password"));
+                if(rs.getString("email").equals(email) && rs.getString("password").equals(pswd)){ 
+                    updateLogin(email);
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al autentificar en la tabla USERS: " + e.getMessage());
+        }
+        return false;
+    }
+
+    private void updateLogin(String email){
+        String Ssql = "UPDATE USERS SET login=? WHERE email=" + email;
+        try (Connection conn = this.connect()){
+            PreparedStatement prest = conn.prepareStatement(Ssql);
+            prest.setBoolean(5, true);
+        } catch (SQLException ex) {
+            Logger.getLogger(HerokuUsersSqlConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public boolean signOff(String email) throws SQLException {
+        String sql = "SELECT DISTINCT id, email FROM USERS WHERE email=" + email;
+        
+        try (Connection conn = this.connect();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)){
+            while (rs.next()) {
+                System.out.println(rs.getString("email"));
+                System.out.println(rs.getString("password"));
+                if(rs.getString("email").equals(email)){ 
+                    signOff2(email);
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al autentificar en la tabla USERS: " + e.getMessage());
+        }
+        return false;
+    }
+
+    private void signOff2(String email){
+        String Ssql = "UPDATE USERS SET login=" + true + "WHERE email=" + email;
+        try (Connection conn = this.connect()){
+            PreparedStatement prest = conn.prepareStatement(Ssql);
+            prest.setBoolean(5, false);
+        } catch (SQLException ex) {
+            Logger.getLogger(HerokuUsersSqlConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
