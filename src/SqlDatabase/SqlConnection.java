82f8403b05dee940f4ff9c6aedbe36aa581cdd45
selenegc01 <99739761+selenegc01@users.noreply.github.com>
2022-03-28 15:48:41 +0100

    Update SqlConnection.java

--- src/SqlDatabase/SqlConnection.java
+++ src/SqlDatabase/SqlConnection.java
@@ -8,18 +8,16 @@ import java.sql.SQLException;
 
 public abstract class SqlConnection {
 
-    protected String url;
-    protected String user;
-    protected String pswd;
+    public static final String url = "jdbc:mysql://i54jns50s3z6gbjt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com/iy5jkxqnaep9jd04" + "?useSSL=false";
+    public static final String user = "e3uv1gsfsu6r8v6j";
+    public static final String pswd = "ud6g593tlbsh3knw";
+    private Connection con = null;
     
-    public void SqlConnection (String url, String user, String pswd){
-        this.url = url;
-        this.user = user;
-        this.pswd = pswd; 
+    public void SqlConnection (){
+
     }
-        
-    public Connection connect() {
-        Connection con = null;
+
+    public Connection getConexion() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection(url, user, pswd);
@@ -28,5 +26,4 @@ public abstract class SqlConnection {
         }
         return con;
     }
-    
 }
