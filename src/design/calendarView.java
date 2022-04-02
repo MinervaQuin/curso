3dd96f3c0872109e8974e17d79e9e64283289fd6
Nauzet Déniz Perdomo <nauuuni@gmail.com>
2022-04-02 13:10:38 +0100

    Update the last changes in calendarView

--- src/design/calendarView.java
+++ src/design/calendarView.java
@@ -1,30 +1,139 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package design;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
+import javax.swing.JOptionPane;
+import model.*;
 
-/**
- *
- * @author nauze
- */
 public class calendarView extends javax.swing.JFrame {
-
-    /**
-     * Creates new form calendarView
-     */
+    
+    private int actualMonth;
+    private int actualYear;
+    private CalendarTask actualCalendar;
+    
+    // Constructor
     public calendarView() {
         initComponents();
-        currentCalendar();
+        test();
+        Calendar cl = new GregorianCalendar();
+        actualMonth = cl.get(Calendar.MONTH);
+        actualYear = cl.get(Calendar.YEAR);
+        updateTasks();
     }
     
-    public void currentCalendar() {
-        Calendar c1 = GregorianCalendar.getInstance();
-        int i = c1.getFirstDayOfWeek();
-        System.out.println(i);
+    // Test para añadir tareas al calendario
+    public void test() {
+        CalendarTask c = new CalendarTask("prueba");
+        Task t = new Task("Esto es una prueba", "This is the description", "22:30", 3);
+        String date = "2022-03-23";
+        c.addTask(date, t);
+        t = new Task("Esto es una prueba1", "This is the description", "22:30", 3);
+        c.addTask(date, t);
+        t = new Task("Esto es una prueba2", "This is the description", "22:30", 3);
+        c.addTask(date, t);
+        t = new Task("Esto es una prueba3", "This is the description", "22:30", 3);
+        c.addTask(date, t);
+        date = "2022-03-24";
+        t = new Task("Esto es una prueba1", "This is the description", "22:30", 3);
+        c.addTask(date, t);
+        date = "2022-03-23";
+        //List l = c.getTasks(date);
+        date = "2022-03-23";
+        for (int i = 0; i < 7; i++) {
+            date = "2022-02-2" + i;
+            t = new Task("En el mes actual", "This is the description", "22:30", 3);
+            c.addTask(date, t);
+            c.addTask(date, t);
+            c.addTask(date, t);
+        }
+        //l = c.getTasks(date);
+        this.actualCalendar = c;
+    }
+    
+    // Método para formatear la tabla y actualizar las tareas
+    public void updateTasks() {
+        jTable1.setRowHeight(80);
+        Object mesAux [][] = calculateDate(actualMonth, actualYear);
+        for (int i = 0; i<mesAux.length;i++) {
+            for (int j = 0; j<mesAux[0].length;j++) {
+                if (mesAux[i][j] == null) {
+                    mesAux[i][j] = new String[]{""};
+                } else {
+                    int day = (Integer)mesAux[i][j];
+                    String date = "";
+                    if (actualMonth <= 9) {
+                        date = actualYear + "-0" + actualMonth;
+                    } else {
+                        date = actualYear + "-" + actualMonth;
+                    }
+                    if (day <= 9) {
+                        date += "-0" + day;
+                    } else {
+                        date += "-" + day;
+                    }
+                    List<Task> tasks = actualCalendar.getTasks(date);
+                    String[] content = new String[tasks.size()+1];
+                    content[0] = Integer.toString(day);
+                    for (int k = 1; k < content.length; k++) {
+                        content[k] = tasks.get(k-1).getName();
+                    }
+                    mesAux[i][j] = content;
+                }
+            }
+        }
+        jTable1.setCellSelectionEnabled(true);
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            mesAux,
+            new String [] {
+                "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"
+            }));
+       MultiLineTableCellRenderer renderer = new MultiLineTableCellRenderer();
+       jTable1.setDefaultRenderer(String[].class, renderer);
+       for (int i = 0; i <7; i++) {
+           jTable1.getColumnModel().getColumn(i).setCellRenderer(renderer);
+       }
+    }
+    
+    // Método para calcular la fecha en la que se encuentra el usuario actualmente
+    private Object[][] calculateDate (int month, int year) {
+        String months[] = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", 
+            "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+        Calendar c1 = new GregorianCalendar(year, month, 1);
+        Object mesAux [][] = getMonth(c1.get(Calendar.DAY_OF_MONTH), c1.get(Calendar.DAY_OF_WEEK), 
+                c1.getActualMaximum(Calendar.DAY_OF_MONTH));
+        this.actualMonth = month;
+        this.actualYear = year;
+        jLabel1.setText(months[month] + " " + year);
+        return mesAux;
+    }
+    
+    // Método para conseguir el mes entero de la fecha
+    private Object [][] getMonth(int dayM, int dayOfWeek, int maxDayOfMonth) {
+        Object mes[][] = {
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null}
+            };
+        dayOfWeek = dayOfWeek-2;
+        if (dayOfWeek == -1) {
+            dayOfWeek = 6;
+        }
+        for (int i = 0; i < 6; i++) {
+            for (int j = dayOfWeek; j < 7; j++) {
+                if (dayM > maxDayOfMonth) {
+                    break;
+                }
+                mes[i][j] = dayM;
+                dayM++;
+            }
+            dayOfWeek = 0;
+        }
+        return mes;
     }
 
     /**
@@ -36,21 +145,225 @@ public class calendarView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        addTaskInternal = new javax.swing.JInternalFrame();
+        jLabel5 = new javax.swing.JLabel();
+        nameField = new javax.swing.JTextField();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        descriptionField = new javax.swing.JTextArea();
+        hourBox = new javax.swing.JComboBox<>();
+        minBox = new javax.swing.JComboBox<>();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jButton3 = new javax.swing.JButton();
+
+        addTaskInternal.setVisible(true);
+
+        jLabel5.setText("Hora:");
+
+        nameField.setText("Añade un título");
+
+        descriptionField.setColumns(20);
+        descriptionField.setRows(5);
+        descriptionField.setText("Añade una descripción");
+        jScrollPane3.setViewportView(descriptionField);
+
+        hourBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24" }));
+
+        minBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60" }));
+        minBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                minBoxActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout addTaskInternalLayout = new javax.swing.GroupLayout(addTaskInternal.getContentPane());
+        addTaskInternal.getContentPane().setLayout(addTaskInternalLayout);
+        addTaskInternalLayout.setHorizontalGroup(
+            addTaskInternalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(addTaskInternalLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(addTaskInternalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(addTaskInternalLayout.createSequentialGroup()
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 325, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(40, 40, 40)
+                        .addComponent(jLabel5)
+                        .addGap(18, 18, 18)
+                        .addComponent(hourBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(minBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(nameField, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(104, 104, Short.MAX_VALUE))
+        );
+        addTaskInternalLayout.setVerticalGroup(
+            addTaskInternalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(addTaskInternalLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(nameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(addTaskInternalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(addTaskInternalLayout.createSequentialGroup()
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 123, Short.MAX_VALUE)
+                        .addGap(131, 131, 131))
+                    .addGroup(addTaskInternalLayout.createSequentialGroup()
+                        .addGroup(addTaskInternalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel5)
+                            .addComponent(hourBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(minBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+        );
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane2.setViewportView(jTextArea1);
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("jLabel1");
+
+        jButton1.setText("Siguiente");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Anterior");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        jButton3.setText("Añadir Tarea");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton2)
+                        .addGap(254, 254, 254)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton1))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 760, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, 109, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton2)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel1)
+                        .addComponent(jButton1)))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 537, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton3)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    // Botón de avanzar al siguiente mes
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        if (this.actualMonth + 1 == 12) {
+            this.actualMonth = 0;
+            this.actualYear += 1;
+        } else {
+            this.actualMonth += 1;
+        }
+        updateTasks();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    // Botón de retroceder al anterior mes
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        if (this.actualMonth - 1 == -1) {
+            this.actualMonth = 11;
+            this.actualYear -= 1;
+        } else {
+            this.actualMonth -= 1;
+        }
+        updateTasks();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        if (jTable1.getSelectedRow() == -1 || jTable1.getSelectedColumn() == -1) {
+            JOptionPane.showConfirmDialog(null,"Selecciona una fecha.","", JOptionPane.PLAIN_MESSAGE, JOptionPane.DEFAULT_OPTION);
+            return;
+        }
+        int option = JOptionPane.showConfirmDialog(null,addTaskInternal,"", JOptionPane.OK_CANCEL_OPTION, JOptionPane.DEFAULT_OPTION, null);
+        switch (option) {
+            case JOptionPane.OK_OPTION:
+                String[] tasks = (String[])jTable1.getValueAt
+                        (jTable1.getSelectedRow(), jTable1.getSelectedColumn());
+                Task t = new Task(nameField.getText(), descriptionField.getText(), 
+                        hourBox.getSelectedItem()+":"+minBox.getSelectedItem(), 3);
+                String month = "";
+                if (this.actualMonth < 10) {
+                    month = "0" + this.actualMonth;
+                } else {
+                    month = Integer.toString(this.actualMonth);
+                }
+                String day = "";
+                if (Integer.parseInt(tasks[0]) < 10) {
+                    day = "0" + tasks[0];
+                } else {
+                    day = tasks[0];
+                }
+                this.actualCalendar.addTask(this.actualYear + "-" + month + "-" + day, t);
+                updateTasks();
+                List l = actualCalendar.getTasks(this.actualYear + "-" + month + "-" + day);
+                t = (Task)l.get(l.size()-1);
+                nameField.setText("Añade un título");
+                descriptionField.setText("Añade una descripcción");
+                hourBox.setSelectedIndex(0);
+                minBox.setSelectedIndex(0);
+                break;
+            case JOptionPane.CANCEL_OPTION:
+                break;
+        }
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void minBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_minBoxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_minBoxActionPerformed
 
     /**
      * @param args the command line arguments
@@ -88,5 +401,20 @@ public class calendarView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JInternalFrame addTaskInternal;
+    private javax.swing.JTextArea descriptionField;
+    private javax.swing.JComboBox<String> hourBox;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JComboBox<String> minBox;
+    private javax.swing.JTextField nameField;
     // End of variables declaration//GEN-END:variables
 }
