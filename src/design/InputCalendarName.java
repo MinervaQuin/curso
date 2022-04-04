3dd7dce2df6418af2691ca28ae9bfa731a2b0e32
selenegc01 <99739761+selenegc01@users.noreply.github.com>
2022-04-04 13:44:15 +0100

    Update InputCalendarName.java

--- src/design/InputCalendarName.java
+++ src/design/InputCalendarName.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package design;
 
 import SqlDatabase.HerokuCalendarPermitSqlConnection;
 import SqlDatabase.HerokuCalendarSqlConnection;
 import SqlDatabase.HerokuUsersSqlConnection;
 import java.awt.Color;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
-import model.*;
 
 /**
  *
